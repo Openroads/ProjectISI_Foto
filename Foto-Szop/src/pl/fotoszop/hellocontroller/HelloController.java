8787df0fda0f98570e35187f8000631166dbff87
Painth <painthing38@gmail.com>
2017-03-19 21:28:30 +0100

    Initial project upload + .gitignore

---
+++ Foto-Szop/src/pl/fotoszop/hellocontroller/HelloController.java
@@ -0,0 +1,18 @@
+package pl.fotoszop.hellocontroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController{
+	
+	@RequestMapping("/welcome")
+	public ModelAndView helloworld(){
+		
+		ModelAndView model = new ModelAndView("HelloPage");
+		model.addObject("msg","Hello guest!");
+		
+		return model;
+	}
+}
