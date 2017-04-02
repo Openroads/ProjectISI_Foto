f6cf3100d7e4391e26ee80f45d9138a4f9cbc7e6
daren <daren22@o2.pl>
2017-04-02 23:44:17 +0200

    no important changes in mock id

--- Foto-Szop/src/pl/fotoszop/mocks/ClientDAOMock.java
+++ Foto-Szop/src/pl/fotoszop/mocks/ClientDAOMock.java
@@ -16,11 +16,11 @@ public class ClientDAOMock implements ClientDAO {
 	Client[] clientExample = 
 		{
 				new Client(1,"Janusz","Podsiadlo","Warszawa 12","95684717896","56565544565","jano@o2.pl"),
-				new Client(1,"Zenek","Wito","Krakow 12","458642","95684717812","zenus@ogmail.pl"),
-				new Client(1,"Franio","Nowak","Skarzynskiego 12","96065254786","56565544565","aadsas@o2.pl"),
-				new Client(1,"Kuba","Kowalski","Witolda 11","96065251236","56565544565","kubson@o2.pl"),
-				new Client(1,"Zdzisek","Zdunczyk","Casdasda 8","96065259996","56565544565","ads@o2.pl"),
-				new Client(1,"Janek","Grak","Nienaszow 5","96065211111","56565544565","jan@o2.pl")
+				new Client(2,"Zenek","Wito","Krakow 12","458642","95684717812","zenus@ogmail.pl"),
+				new Client(3,"Franio","Nowak","Skarzynskiego 12","96065254786","56565544565","aadsas@o2.pl"),
+				new Client(4,"Kuba","Kowalski","Witolda 11","96065251236","56565544565","kubson@o2.pl"),
+				new Client(5,"Zdzisek","Zdunczyk","Casdasda 8","96065259996","56565544565","ads@o2.pl"),
+				new Client(6,"Janek","Grak","Nienaszow 5","96065211111","56565544565","jan@o2.pl")
 		};
 	
 	this.clients.addAll(Arrays.asList(clientExample));
