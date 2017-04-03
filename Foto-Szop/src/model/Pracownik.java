fae3be24431a7fee959e5888414b612bbf9bbcbd
Piotr Makowiec <pieterm4@wp.pl>
2017-04-04 00:41:58 +0200

    Very important changes

--- Foto-Szop/src/model/Pracownik.java
+++ Foto-Szop/src/model/Pracownik.java
@@ -1,5 +1,12 @@
 package model;
 
-public abstract class Pracownik {
+public abstract class Pracownik 
+{
+  public String imie;
+  public String nazwisko;
+  public String adres;
+  public String pesel;
+  public String nrTelefonu;
+  public String email;
 
 }
