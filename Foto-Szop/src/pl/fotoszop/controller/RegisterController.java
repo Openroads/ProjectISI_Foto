dd0852f87122d3d4303d0f284d47fd41137807b0
daren <daren22@o2.pl>
2017-04-04 09:36:49 +0200

    powrot projekt-zamieszanie

--- Foto-Szop/src/pl/fotoszop/controller/RegisterController.java
+++ Foto-Szop/src/pl/fotoszop/controller/RegisterController.java
@@ -25,11 +25,11 @@ public class RegisterController {
 	@RequestMapping("/addClient")
 	public ModelAndView processForm(@ModelAttribute Form form){
 		
-		ClientDAO database = new ClientDAODbImpl();
+		ClientDAO database = clientDatabaseDAO;
 		boolean flag = checkDatabase(form,database);
 		if(!flag){
 			
-			Client newClient = new Client(2,form.getName(),form.getSurname(),form.getAddress(),form.getIdentityNumber(),form.getPhoneNumber(),form.getEmail());
+			Client newClient = new Client(10,form.getName(),form.getSurname(),form.getAddress(),form.getIdentityNumber(),form.getPhoneNumber(),form.getEmail());
 			database.saveOrUpdate(newClient);
 			ModelAndView model = new ModelAndView("success");
 			model.addObject("client",newClient);
