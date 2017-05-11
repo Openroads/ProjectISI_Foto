edd1c2f8838e88432de39772cc81a2f657bfe92a
mariuszgr94 <mariusz.gr94@gmail.com>
2017-05-11 14:14:49 +0200

    Dodanie testów jednostkowych dla Form

---
+++ Foto-Szop/src/pl/fotoszop/tests/TestForm.java
@@ -0,0 +1,34 @@
+package pl.fotoszop.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import pl.fotoszop.model.Form;
+
+public class TestForm {
+	
+	@Test
+	public void testCheckPassword(){
+		Form f = new Form();
+		f.setPassword("Haslo123");
+		f.setPassword2("Haslo123");
+		assertEquals(true, f.checkPasswords());
+		f.setPassword("123HasloHaslo");
+		assertEquals(false, f.checkPasswords());
+	}
+	
+	@Test 
+	public void testHashGenerator(){
+		Form f = new Form();
+		f.setPassword("Haslo");
+		f.setPassword2("Haslo");
+		String s = f.getPassword();
+		f.doHash();
+		assertFalse(s.equals(f.getPassword()));
+	}
+	
+	/*@Test
+	public void */
+
+}
