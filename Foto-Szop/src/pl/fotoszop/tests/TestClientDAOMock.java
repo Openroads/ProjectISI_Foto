7f3bad2f80c0958f70ff0843fa617ab9141f80fa
mariuszgr94 <mariusz.gr94@gmail.com>
2017-05-15 22:09:13 +0200

    Testy jednostkowe dla klasy TestClientDAOMock.java

---
+++ Foto-Szop/src/pl/fotoszop/tests/TestClientDAOMock.java
@@ -0,0 +1,40 @@
+package pl.fotoszop.tests;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+
+
+import pl.fotoszop.mocks.ClientDAOMock;
+import pl.fotoszop.model.Client;
+import pl.fotoszop.modelinterfaces.IClient;
+
+public class TestClientDAOMock {
+	
+	@Test
+	public void testDelete(){
+		ArrayList<Client> clients = new ArrayList<Client>();
+		ClientDAOMock clientsDao = new ClientDAOMock();
+		assertTrue(clientsDao.delete(1));
+	}
+	
+	@Test
+	public void testGetClientById(){
+		ArrayList<Client> clients = new ArrayList<Client>();
+		ClientDAOMock clientsDao = new ClientDAOMock();
+		IClient c = clientsDao.getClientById(1);
+		assertEquals("Janusz", c.getName());
+	}
+	
+	@Test
+	public void testSaveOrUpdate(){
+		Client c = new Client(0,"Jeremiasz","Jurkowski","Nienaszow 5","96065211111","56565544565","jan@o2.pl");
+		ArrayList<Client> clients = new ArrayList<Client>();
+		//autoinkrementacja programowa w ClientDAOMock
+		ClientDAOMock clientsDao = new ClientDAOMock();
+		assertEquals(7, clientsDao.saveOrUpdate(c));
+	}
+}
