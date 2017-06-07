38e0e76345c77c6aad4e5e342c388d8bd19f1415
Painth <painthing38@gmail.com>
2017-06-07 16:55:51 +0200

    Added history view initialization

---
+++ Foto-Szop/src/pl/fotoszop/controller/HistoryController.java
@@ -0,0 +1,34 @@
+package pl.fotoszop.controller;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import pl.fotoszop.DAODbImpl.ClientDAODbImpl;
+import pl.fotoszop.dto.Form;
+import pl.fotoszop.mocks.ClientDAOMock;
+import pl.fotoszop.model.Client;
+import pl.fotoszop.modelinterfaces.IClient;
+
+@Controller
+public class HistoryController{
+	
+//	@Autowired
+//	private ClientDAODbImpl clientDatabaseDAO;
+	
+	@RequestMapping("/history")
+	public ModelAndView helloworld(){
+		ModelAndView model = new ModelAndView("history");
+		return model;
+	}
+	
+	
+	
+	
+	
+}
